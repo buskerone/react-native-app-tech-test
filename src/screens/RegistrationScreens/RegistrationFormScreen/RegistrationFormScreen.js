738947a7842a989df66d4fab4701cc8a27c1f0cc
Carlos Knopel <carlos.knopel@gmail.com>
2020-01-15 00:10:27 -0300

    Store setup. Add redux-logger.

--- src/screens/RegistrationScreens/RegistrationFormScreen/RegistrationFormScreen.js
+++ src/screens/RegistrationScreens/RegistrationFormScreen/RegistrationFormScreen.js
@@ -3,6 +3,6 @@ import { View } from 'react-native';
 
 export default function RegistrationFormScreen() {
   return(
-    <View style={{ backgroundColor: 'blue' }} />
+    <View style={{ flex: 1, backgroundColor: 'blue' }} />
   );
 }
