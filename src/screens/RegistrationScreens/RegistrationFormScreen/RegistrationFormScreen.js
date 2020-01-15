c3f8372ffbbca2bfb46b4e8857b9f773688b1721
Carlos Knopel <carlos.knopel@gmail.com>
2020-01-15 00:46:56 -0300

    User react-hooks-form. Improve UI registration form screen.

--- src/screens/RegistrationScreens/RegistrationFormScreen/RegistrationFormScreen.js
+++ src/screens/RegistrationScreens/RegistrationFormScreen/RegistrationFormScreen.js
@@ -1,8 +1,78 @@
 import React from 'react';
-import { View } from 'react-native';
+import {
+  View,
+  SafeAreaView,
+  StatusBar,
+  TextInput,
+  TouchableOpacity,
+  Text,
+} from 'react-native';
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
+import { useForm, Controller } from "react-hook-form";
+import styles from './styles';
 
 export default function RegistrationFormScreen() {
+  const { register, setValue, errors, triggerValidation, control, handleSubmit } = useForm();
+
+  const onSubmit = data => {
+    console.log(data);
+  };
+
+  const onChange = args => {
+    return {
+      value: args[0].nativeEvent.text,
+    };
+  };
+
   return(
-    <View style={{ flex: 1, backgroundColor: 'blue' }} />
+    <>
+      <StatusBar barStyle="dark-content" />
+      <KeyboardAwareScrollView style={styles.container}>
+        <SafeAreaView>
+          <Controller
+            as={<TextInput placeholder="Nombre" style={styles.input} />}
+            onChange={onChange}
+            control={control}
+            name="firstName"
+          />
+          <Controller
+            as={<TextInput placeholder="Apellido" style={styles.input} />}
+            name="lastName"
+            control={control}
+            onChange={onChange}
+          />
+          <Controller
+            as={<TextInput placeholder="Teléfono" style={styles.input} />}
+            name="phoneNumber"
+            control={control}
+            onChange={onChange}
+          />
+          <Controller
+            as={<TextInput placeholder="Correo electrónico" style={styles.input} />}
+            name="email"
+            control={control}
+            onChange={onChange}
+          />
+          <Controller
+            as={<TextInput placeholder="Contraseña" style={styles.input} />}
+            name="password"
+            control={control}
+            onChange={onChange}
+          />
+          <Controller
+            as={<TextInput placeholder="Dirección" style={styles.input} />}
+            name="address"
+            control={control}
+            onChange={onChange}
+          />
+          <View style={styles.termsAndConditionsContainer}>
+            <Text style={styles.termsAndConditionsText}>Al presionar "Registrarme" estás de acuerdo con nuestros términos y condiciones de uso</Text>
+          </View>
+          <TouchableOpacity style={styles.buttonContainer}>
+            <Text style={styles.buttonText}>REGISTRARME</Text>
+          </TouchableOpacity>
+        </SafeAreaView>
+      </KeyboardAwareScrollView>
+    </>
   );
 }
---
+++ src/screens/RegistrationScreens/RegistrationFormScreen/styles.js
@@ -0,0 +1,40 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  container: {
+    backgroundColor: 'white',
+  },
+  buttonContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 20,
+    height: 40,
+    backgroundColor: '#009E9D',
+    borderRadius: 4,
+    margin: 20,
+  },
+  buttonText: {
+    fontSize: 12,
+    fontWeight: 'bold',
+    color: 'white'
+  },
+  termsAndConditionsContainer: {
+    margin: 20,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  termsAndConditionsText: {
+    color: 'black',
+    textAlign: 'center',
+  },
+  input: {
+    backgroundColor: 'white',
+    borderBottomColor: '#DDE1E5',
+    borderBottomWidth: 1,
+    height: 40,
+    padding: 10,
+    marginTop: 10,
+    margin: 20,
+    borderRadius: 4,
+  }
+});
