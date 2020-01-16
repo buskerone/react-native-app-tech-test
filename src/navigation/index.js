c68745d1c49a0bb5e9330b3802c506e624ede200
Carlos Knopel <carlos.knopel@gmail.com>
2020-01-15 23:03:49 -0300

    Nav bar style refactor

--- src/navigation/index.js
+++ src/navigation/index.js
@@ -3,25 +3,24 @@ import { createStackNavigator } from 'react-navigation-stack';
 import RegistrationFormScreen from '../screens/RegistrationScreens/RegistrationFormScreen';
 import RegistrationLookScreen from '../screens/RegistrationScreens/RegistrationLookScreen';
 
+const headerTitleStyle = {
+  color: '#009E9D',
+  fontSize: 13,
+};
+
 const AppNavigator = createStackNavigator({
   SelectLook: {
     screen: RegistrationLookScreen,
     navigationOptions: () => ({
       title: 'PELO, MAQUILLAJE',
-      headerTitleStyle: {
-        color: '#009E9D',
-        fontSize: 13,
-      },
+      headerTitleStyle
     }),
   },
   RegisterForm: {
     screen: RegistrationFormScreen,
     navigationOptions: () => ({
       title: 'REGISTRO',
-      headerTitleStyle: {
-        color: '#009E9D',
-        fontSize: 13,
-      },
+      headerTitleStyle
     }),
   },
 }, {
