516acd5b583fa4eb97c5f40b681365a88ab92406
Carlos Knopel <carlos.knopel@gmail.com>
2020-01-30 18:18:36 -0300

    Change app name

--- android/app/src/main/java/com/carlosknopel/techtest/MainActivity.java
+++ android/app/src/main/java/com/carlosknopel/techtest/MainActivity.java
@@ -1,4 +1,4 @@
-package com.belin;
+package com.carlosknopel.techtest;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "Belin";
+    return "TechTest";
   }
 }
--- android/app/src/main/java/com/carlosknopel/techtest/MainApplication.java
+++ android/app/src/main/java/com/carlosknopel/techtest/MainApplication.java
@@ -1,4 +1,4 @@
-package com.belin;
+package com.carlosknopel.techtest;
 
 import android.app.Application;
 import android.content.Context;
